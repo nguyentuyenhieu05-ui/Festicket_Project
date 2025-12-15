af99bdfdbf373d7b0626e47e9b6ba7908b9ce391
nguyentuyenhieu05-ui <nguyentuyenhieu05@gmail.com>
2025-12-15 18:04:17 +0700

    Thay đổi nhỏ

--- phuongthucthanhtoan/phuongthucthanhtoan.js
+++ phuongthucthanhtoan/phuongthucthanhtoan.js
@@ -19,7 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
     let paymentInterval = null; // Biến để lưu trữ ID của setInterval
     const CHECK_INTERVAL = 2000; // 2 giây kiểm tra 1 lần
 
-    
     const GOOGLE_SHEET_API_URL = 'https://script.google.com/macros/s/AKfycbwshuvDEGA1jz6m6tyBuvMXxbmpblqa0Vw9QqenLHUOpp5CGmLO82V9ELBO6_JhdhRarg/exec'; 
     
     const BANK_ACCOUNT_NO = '0966980458'; 
